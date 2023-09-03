d0ba741dce38bf36cc905c72eafc6e10b0e8c311
NailLegProcessorDivide <github@whytho.com>
2023-09-03 04:05:14 +0100

    test file loader

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,39 @@
-use joes_gdmc_http_rs;
-use tokio;
+use std::{error::Error, path::PathBuf};
 
-#[tokio::main]
-async fn main() {
+use joes_gdmc_http_rs::{self, net_loader::NetLoader, WorldLoader, world::{coords::{V2di32, Vec3D}, blocks::Blocks}, file_loader::FileLoader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // let mut loader = NetLoader::new("http://127.0.0.1:9000".to_string());
+    let mut loader = FileLoader::new_from_file(&PathBuf::from("New World"))?;
+    loader.load_chunks(V2di32::new(-60, -60), 100.into())?;
+
+    let world = loader.get_world();
     //let res = joes_gdmc_http_rs::get_block(0, -10, 0).await.unwrap();
     //println!("Hello, world! {}!", joes_gdmc_http_rs::blocks::block_to_string(&res));
-    let mut res = vec![];
-    let chunks = match joes_gdmc_http_rs::get_chunk_nbt(0, 0, 4, 4).await {
-        Ok(c) => c,
-        Err(s) => panic!("{}", s),
-    };
-    for chunk in chunks {
-        for y in 1..=384 {
-            let sy = (384 - y) % 16;
-            let by = (384 - y) / 16;
-            if chunk.sub_chunks[sy].blocks[4][by][4] != joes_gdmc_http_rs::blocks::Blocks::Air {
-                println!(
-                    "{} {} {} {:?}",
-                    chunk.x * 16 + 4,
-                    384 - y as isize - 64,
-                    chunk.z * 16 + 4,
-                    chunk.sub_chunks[sy].blocks[4][by][4]
-                );
-                break;
-            }
-        }
-    }
 
-    let start = 740;
-    let mut fut = joes_gdmc_http_rs::get_chunk_nbt(0, start, 1, 1);
-    for x in 1..1 {
-        let next = joes_gdmc_http_rs::get_chunk_nbt(0, start + x, 64, 1);
-        let mut chunks = match fut.await {
-            Ok(c) => c,
-            Err(s) => panic!("{}", s),
-        };
-        fut = next;
-        res.append(&mut chunks);
-    }
-    let mut chunks = match fut.await {
-        Ok(c) => c,
-        Err(s) => panic!("{}", s),
-    };
-    res.append(&mut chunks);
+    println!("0 {:?}", world.get_block(Vec3D::new(1, 0, 0))?);
+    println!("-1 {:?}", world.get_block(Vec3D::new(0, -1, 0))?);
+    println!("-2 {:?}", world.get_block(Vec3D::new(0, -2, 0))?);
+    println!("-3 {:?}", world.get_block(Vec3D::new(0, -3, 0))?);
+    println!("-4 {:?}", world.get_block(Vec3D::new(0, -4, 0))?);
+    println!("-5 {:?}", world.get_block(Vec3D::new(0, -5, 0))?);
+    // for x in 0..4 {
+    //     for z in 0..4 {
+    //         for y in (-64..320).rev() {
+    //             let block = world.get_block(Vec3D::new(x, y, z))?;
+    //             if block != Blocks::Air {
+    //                 println!(
+    //                     "{} {} {} {:?}",
+    //                     x,
+    //                     y,
+    //                     z,
+    //                     block
+    //                 );
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+    Ok(())
     //println!("Hello, world! {:?}!", res);
 }
